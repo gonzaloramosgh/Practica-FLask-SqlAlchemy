29be74cf69a55aaf4384d27e18a22d8ba7fb5474
Gonzalo Ramos <110637953+gonzaloramosgh@users.noreply.github.com>
2023-04-02 17:23:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def index():
     finally:
         return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
